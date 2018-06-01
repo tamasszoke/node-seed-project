bf931fa4e3b4edb554b89d92bb216daee9ea0977
Tamas Szoke <tamas.szoke.dev@gmail.com>
2018-06-01 11:05:04 +0200

    Update socket.io cheatsheet

--- controllers/socket.js
+++ controllers/socket.js
@@ -28,23 +28,30 @@ module.exports = (root) => {
 	});
 };
 
-/* Socket.io cheatsheet
+/**
+	// Socket.IO Cheatsheet
 
-	Send to current request socket client:
+	// Add socket to room
+	socket.join('some room');
+
+	// Remove socket from room
+	socket.leave('some room');
+
+	// Send to current client
 	socket.emit('message', 'this is a test');
 
-	Send to all clients, include sender:
+	// Send to all clients include sender
 	io.sockets.emit('message', 'this is a test');
 
-	Send to all clients except sender:
+	// Send to all clients except sender
 	socket.broadcast.emit('message', 'this is a test');
 
-	Send to all clients in 'game' room(channel) except sender:
+	// Send to all clients in 'game' room(channel) except sender
 	socket.broadcast.to('game').emit('message', 'this is a test');
 
-	Send to all clients in 'game' room(channel), include sender:
+	// Send to all clients in 'game' room(channel) include sender
 	io.sockets.in('game').emit('message', 'this is a test');
 
-	Send to individual socketid:
-	io.sockets.socket(socketid).emit('message', 'this is a test');
+	// Send to individual socket id
+	io.sockets.socket(socketId).emit('message', 'this is a test');
 */
